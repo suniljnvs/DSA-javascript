48e7b739a6968b7e783e08604cb06ed934256ca3
suniljnvs <sunikumar0551998@gmail.com>
2022-09-09 13:25:35 +0530

    done

--- src/find Missing no.js
+++ src/find Missing no.js
@@ -1,41 +1,6 @@
 //  Find the missing number from sorted array with O(n) complexity
 
 
-// function findMissing(arr){
-//     let array = arr.sort((a,b)=> a-b) // when array are unsorted
-//     let missing = [];
-    
-//     for(let i=0 ; i<array.length-1; i++){
-//         if(!(array[i+1] - array[i] == 1)){
-
-//             missing.push(array[i]+1)
-//         }
-//     }
-//     return missing;
-// }
-// const res = findMissing([1,6,8,9]);
-// console.log(res);  // [ 2, 5, 7 ]
-
-
-
-// function findMissing(arr){
-//         let array = arr.sort((a,b)=> a-b) // when array are unsorted
-//         let missing = [];
-//         let sum =0;
-//         for(let i=0 ; i<array.length-1; i++){
-
-//             sum = sum +1
-//             if(sum !== arr[i]){
-//                 missing.push(sum)
-//             }
-//         }
-//         return missing;
-//     }
-//     const res = findMissing([1,6,8,9]);
-//     console.log(res);  // [ 2, 5, 7 ]
-
-
-
 function missing (arr){
     // let array = arr.sort((a,b)=> a-b) 
     arr.sort(function(a,b){
